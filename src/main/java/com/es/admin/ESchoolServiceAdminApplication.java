570c7f873642135e49c9dea39dd56c1366e8224e
WeisonWei <weixx3@126.com>
2020-04-03 14:24:43 +0800

    update

--- src/main/java/com/es/admin/ESchoolServiceAdminApplication.java
+++ src/main/java/com/es/admin/ESchoolServiceAdminApplication.java
@@ -3,9 +3,10 @@ package com.es.admin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 @EnableAdminServer
 public class ESchoolServiceAdminApplication {
